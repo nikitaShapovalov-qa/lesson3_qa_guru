11754643846a0a135bda96e97837fdcd24bb03ad
Nikita Shapovalov <nikitka58rus2013@yandex.ru>
2025-06-19 01:34:22 +0300

    example_test_added

---
+++ src/test/java/exampleTestLesson3.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTestLesson3 {
+
+    @Test
+    void newTest(){
+        Assertions.assertTrue(3>2);
+    }
+}
